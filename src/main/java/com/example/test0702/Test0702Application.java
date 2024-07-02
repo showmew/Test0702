24070f84da6e147217be89a9a6463edca0b9fc62
showmew <2140692607@qq.com>
2024-07-02 09:16:50 +0800

    初始化

---
+++ src/main/java/com/example/test0702/Test0702Application.java
@@ -0,0 +1,13 @@
+package com.example.test0702;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test0702Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Test0702Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/test0702/Test0702ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.test0702;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Test0702ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
